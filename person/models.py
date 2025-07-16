5608dc286a7485a6e3308d60914082224252dbaa
Denis <work80@mail.ru>
2025-07-16 17:50:07 +0700

    [master]: New migrations was added for the User db

--- person/models.py
+++ person/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+import datetime
 # Create your models here.
 class Users(AbstractUser):
-    pass
+    is_verified = models.BooleanField(default=False)
+    verification_code = models.CharField(max_length=6, blank=True, null=True)
+    balance = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, )
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "User: %s Regisrated was: %s" %  (self.username, self.created_at)
+
+    class Meta(AbstractUser.Meta):
+        indexes = [models.Index(fields=["is_active"])]
