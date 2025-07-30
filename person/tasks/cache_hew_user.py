86a3bee6fd3be94ac23f33033bc54ec32e4c4108
Denis <work80@mail.ru>
2025-07-30 13:44:47 +0700

    [dev]: person_upgrade_data_of_user & task_user_from_cache was added

--- person/tasks/cache_hew_user.py
+++ person/tasks/cache_hew_user.py
@@ -30,12 +30,13 @@ logger = get_task_logger(__name__)
     bind=False,
     ignore_result=True,
     autoretry_for=(TimeoutError,),
-    retry_backoff=30,
+    retry_backoff=False,
 )
 def task_postman_for_user_id(self, user_id_list: list[int]) -> TypeUser:
     """
-    retry_backoff - It's time for a wait before re-sending.
+    retry_backoff - https://docs.celeryq.dev/en/stable/userguide/tasks.html#Task.autoretry_for
     ignore_result - If False, it means we don't have a response. Or conversely if we have a True.
+    https://docs.celeryq.dev/en/stable/userguide/tasks.html#Task.ignore_result
 
     This Task is a POSTMAN. Here only transmits the user id. It happens from the entry point to the handler function
     :param self:
@@ -66,6 +67,7 @@ def person_to_redis(user_id_list: list[int]) -> TypeUser:
         if not client.ping():
             raise ConnectionError("Redis connection failed")
         log.info("Client is ping")
+        # Basis db
         person_list = Users.objects.filter(id=user_id_list[0])
         if not person_list.exists():
             raise ValueError(
@@ -75,10 +77,14 @@ def person_to_redis(user_id_list: list[int]) -> TypeUser:
         user_serializer = CacheUsersSerializer(person_list[0])
         user_dict: TypeUser = user_serializer.data.copy()
         log.info("Received user ID: %s" % user_dict["id"])
-        client.set(f"user:{str(user_dict["id"])}:person", json.dumps(user_dict), 86400)
+        # Redis's cache
+        # Now will be saving on the 27 hours.
+        # 'task_user_from_cache' task wil be to upgrade postgres at ~ am 01:00
+        # Timetable look the 'project.celery.app.base.Celery.conf'
+        client.set(f"user:{str(user_dict["id"])}:person", json.dumps(user_dict), 97200)
         client.close()
         log.info("User with %s ID was saved in Redis. The End" % str(user_dict["id"]))
         return user_dict
     except Exception as error:
-        logger.error("[%s]: ERROR => %s" % (__name__, error.args[0]))
+        log.error("[%s]: ERROR => %s" % (__name__, error.args[0]))
         raise ValueError("[%s]: ERROR => %s" % (__name__, error.args[0]))
--- project/celery.py
+++ project/celery.py
@@ -1,14 +1,11 @@
 import json
 import os
 
-from celery import Celery, shared_task
+from celery import Celery
+from celery.schedules import crontab
 
 from project import celeryconfig
-import redis
-from redis.retry import Retry
-from redis.backoff import ExponentialBackoff
 
-from stalk.tasks_generations.first_task import logger
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
@@ -29,27 +26,9 @@ app = Celery(
 app.config_from_object(celeryconfig)
 # Load task modules from all registered Django apps.
 # app.autodiscover_tasks()
-
-
-@shared_task(name=__name__, bind=True, ignore_result=False)
-def debug_task(self, person_data):
-    try:
-        # Устойчивое подключение
-        redis_client = redis.Redis(
-            host="83.166.245.209", port=6380, socket_timeout=10, retry_on_timeout=True
-        )
-
-        if not redis_client.ping():
-            raise ConnectionError("Redis connection failed")
-
-        # Атомарная запись
-        with redis_client.pipeline() as pipe:
-            pipe.set(
-                f"user:{person_data['id']}:person", json.dumps(person_data), ex=86400
-            )
-            pipe.execute()
-
-    except Exception as e:
-        logger.error(f"Redis operation failed: {str(e)}")
-        raise self.retry(exc=e)
-    print(f"Request: {self.request!r}")
+app.conf.beat_schedule = {
+    "midnigth-tast": {
+        "task": "person.tasks.task_user_from_cache_to_td_repeat.task_user_from_cache",
+        "schedule": crontab(hour=1, minute=0),
+    }
+}
